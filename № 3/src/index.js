79f29170da3d837e73f21275d14c77bcf46e9006
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-01-26 11:44:09 +0300

    Update index.js

--- № 3/src/index.js
+++ № 3/src/index.js
@@ -1,23 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
+function quadraticEquation(a,b,c) {
+    let x1, x2, discriminant;
 
-    function quadraticEquation(a,b,c) {
-        let x1, x2, discriminant;
+    discriminant = b**2 - 4 * a * c;
 
-        discriminant = b**2 - 4 * a * c;
-
-        if (discriminant > 0) { // квадратное уравнение имеет два разных корня
-            x1 = (-b + discriminant**0.5) / (2 * a);
-            x2 = (-b - discriminant**0.5) / (2 * a);
-        } else if (discriminant === 0) { // квадратное уравнение имеет два одинаковых корня
-            x1 = -b / (2 * a);
-            x2 = x1;
-        } else if (discriminant < 0) {
-            return 'Квадратное уравнение не имеет корней';
-        }
-
-        return `x1 = ${x1}, x2 = ${x2}`;
+    if (discriminant > 0) { // квадратное уравнение имеет два разных корня
+        x1 = (-b + discriminant**0.5) / (2 * a);
+        x2 = (-b - discriminant**0/5) / (2 * a);
+    } else if (discriminant === 0) { // квадратное уравнение имеет два одинаковых корня
+        x1 = -b / (2 * a);
+        x2 = x1;
+    } else if (discriminant < 0) {
+        return []; // Квадратное уравнение не имеет корней
     }
 
-    console.log(quadraticEquation(1,8,16)); // x1 = -4, x2 = -4
-    
-});
+    return [x1, x2];
+}
+
+const [x1, x2] = quadraticEquation(1, 8, 16); // 1, 8, 16
+
+console.log(`x1 = ${x1}, x2 = ${x2}`); // x1 = -4, x2 = -4
