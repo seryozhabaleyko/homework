f6bbf5745d06d73249f0d1df07316da2cc6dd784
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-01-29 14:23:56 +0300

    Update index.js

--- № 5/src/index.js
+++ № 5/src/index.js
@@ -1,1 +1,19 @@
+const arr = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8]; // sum 50
 
+const sum = function sumArrayElements(array) {
+
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) { // for, forEach(), reduce()
+
+        const item = array[i];
+
+        if (Array.isArray(item)) {
+            sum += sumArrayElements(item);
+        } else {
+            sum += item;
+        }
+    }
+
+    return sum;
+};
