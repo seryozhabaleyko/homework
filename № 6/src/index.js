64a2ecb8cf0fe678703f358eadb73bf153e05584
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-01-30 17:51:14 +0300

    Update index.js

--- № 6/src/index.js
+++ № 6/src/index.js
@@ -21,16 +21,20 @@ unique(array); // -> [1,2,0]
 unique(array2); // -> [1,2,3]
 unique(array3); // -> [1]
 
-// 
+// Инкапсулируйте условия
 function isUnique(arr, i) {
     return arr.indexOf(arr[i]) === i;
 }
 
 function getUnique(arr) {
     let result = [];
+      
     for (let i = 0; i < arr.length; i++) { // for and Array.forEach() and Array.filter()
+          
         if (isUnique(arr, i)) {
+              
             result.push(arr[i]);
+              
         }        
     }
 
