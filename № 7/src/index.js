83909716bc75f1ae5fcaa5ed3698634a09b261d2
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-01-31 14:57:34 +0300

    Update index.js

--- № 7/src/index.js
+++ № 7/src/index.js
@@ -2,54 +2,21 @@ const isObject = (obj) => obj && typeof obj === 'object';
 const isArray = (obj) => Array.isArray(obj);
 
 function deepEquals(a, b) {
-    let result = false;
-
-    if (isObject(a)) {
-        result = isObjEquals(a, b);
-    } else {
-        if (isArray(a)) {
-            result = isArrayEquals(a, b);
-        } else {
-            result = a === b;
-        }
+    
+    if (a === b) {
+        return true;
     }
 
-    return result;
-}
-
-
-function isObjEquals(a, b) {
-    let result = isObject(a) && isObject(b);
+    let result = isObject(a) && isObject(b) && Object.keys(a).length === Object.keys(b).length;
 
     if (result) {
-        result = Object.keys(a).length === Object.keys(b).length;
-        if (result) {
-            for (const key in a) {
-                if (!deepEquals(a[key], b[key])) {
-                    result = false;
-                    break;
-                }
+        for (const key in a) {
+            if (!deepEquals(a[key], b[key])) {
+                result = false;
+                break;
             }
         }
-    }
-
-    return result;
-}
-
-function isArrayEquals(a, b) {
-    let result = isArray(a) && isArray(b);
-
-    if (result) {
-        result = a.length === b.length;
-        if (result) {
-            for (let i = 0; i < a.length; i++) {
-                if (!deepEquals(a[i], b[i])) {
-                    result = false;
-                    break;
-                }
-            }
-        }
-    }
+    } 
 
     return result;
 }
