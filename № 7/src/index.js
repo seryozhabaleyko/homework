fe5a1e9c42c8c3657d2c4c0766ab1766f8e01b6f
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-01-31 12:50:36 +0300

    Update index.js

--- № 7/src/index.js
+++ № 7/src/index.js
@@ -1,1 +1,63 @@
+const isObject = (obj) => obj && obj.constructor === Object;
+const isArray = (obj) => Array.isArray(obj);
+
+function deepEquals(a, b) {
+    let result = false;
+
+    if (isObject(a)) {
+        result = isObjEquals(a, b);
+    } else {
+        if (isArray(a)) {
+            result = isArrayEquals(a, b);
+        } else {
+            result = a === b;
+        }
+    }
+
+    return result;
+}
+
+function isObjEquals(a, b) {
+    let result = isObject(a) && isObject(b);
+
+    if (result) {
+        result = Object.keys(a).length === Object.keys(b).length;
+        if (result) {
+            for (const key in a) {
+                if (!deepEquals(a[key], b[key])) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+function isArrayEquals(a, b) {
+    let result = isArray(a) && isArray(b);
+
+    if (result) {
+        result = a.length === b.length;
+        if (result) {
+            for (const i = 0; i < a.length; i++) {
+                if (!deepEquals(a[i], b[i])) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+
+let a = { value: { a: 13 }, b: 'abc' };
+let b = { value: { a: 13 }, b: 'abc' };
+
 const deepEqualsOrderMatters = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+
+deepEquals(a, b); // true
+deepEqualsOrderMatters(a, b); // true
