51e17e3259cdf331171a412ac8ae204d343fdfba
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-01-31 12:56:26 +0300

    Update index.js

--- № 5/src/index.js
+++ № 5/src/index.js
@@ -1,7 +1,7 @@
 const arr = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8]; // sum 50
 
 const sum = function sumArrayElements(array) {
-
+    
     let sum = 0;
 
     for (let i = 0; i < array.length; i++) { // for, forEach(), reduce()
@@ -17,3 +17,5 @@ const sum = function sumArrayElements(array) {
 
     return sum;
 };
+
+sum(arr); // сумма 50
