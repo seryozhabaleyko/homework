57c19ef292702d92791c7be79f1d73b150a53949
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-02-06 15:52:15 +0300

    Update bind.js

--- № 9/bind.js
+++ № 9/bind.js
@@ -5,3 +5,18 @@ const sum = (a, b) => a + b;
 const foo = mybind(sum, null, 3, 3);
 
 foo(2,3,4,22,2); // 6
+
+
+var mybind = function mybind(func, context) {
+    for (var _len = arguments.length, args = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+        args[_key - 2] = arguments[_key];
+    }
+
+    return function () {
+        for (var _len2 = arguments.length, rest = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+            rest[_key2] = arguments[_key2];
+        }
+
+        return func.apply(context, [].concat(args, rest));
+    };
+};
