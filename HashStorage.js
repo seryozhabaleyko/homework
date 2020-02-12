6db7efa0cadd8e8757e2c4f8eeb4b235d004b79b
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-02-12 14:14:58 +0300

    Update HashStorage.js

--- HashStorage.js
+++ HashStorage.js
@@ -1,1 +1,60 @@
-🖕 
+function HashStorage() {
+    this._hash = {};
+
+    this.addValue = function (key, value) {
+        this._hash[key] = value;
+    };
+
+    this.getValue = function (key) {
+        return this._hash[key];
+    };
+
+    this.deleteValue = function (key) {
+        return this._hash[key] ? delete this._hash[key] : false;
+    };
+
+    this.getKey = function () {
+        return Object.keys(this._hash);
+    };
+}
+
+const drinkStorage = new HashStorage();
+
+function Drinkables() {
+    HashStorage.call(this);
+
+    this.updateDrink = function (key, value) {
+        if (this._hash[key]) {
+            this._hash[key] = value;
+        }
+    };
+
+    this.isDrink = function (key) {
+        return this._hash[key] ? 'да' : 'нет';   
+    };
+}
+
+const drink = new Drinkables();
+
+function Fruit() {
+    HashStorage.call(this);
+
+    let box = [];
+
+    const parentAddValue = this.addValue;
+    
+    this.addValue = function (key, value) {
+        parentAddValue.call(this, key, value);
+        box.push({[key]: value});
+    };
+
+    this.numberFruit = function () {
+        return Object.keys(this._hash).length;
+    };
+    
+    this.getFruitBox = function () {
+        return box;
+    };
+}
+
+const fruit = new Fruit();
