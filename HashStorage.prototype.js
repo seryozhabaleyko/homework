38a44c2cecaf7c39a325beee5a6cad9e67579cbc
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-02-12 14:36:23 +0300

    Create HashStorage.prototype.js

---
+++ HashStorage.prototype.js
@@ -0,0 +1,64 @@
+function _inherits(subclass, superclass) {
+    subclass.prototype = Object.create(superclass.prototype);
+    subclass.prototype.constructor = subclass;
+}
+
+function HashStorage() {
+    this._hash = {};
+}
+
+HashStorage.prototype.addValue = function (key, value) {
+    this._hash[key] = value;
+};
+
+HashStorage.prototype.getValue = function (key) {
+    return this._hash[key];
+};
+
+HashStorage.prototype.deleteValue = function (key) {
+    return this._hash[key] ? delete this._hash[key] : false;
+};
+
+HashStorage.prototype.getKey = function () {
+    return Object.keys(this._hash);
+};
+
+_inherits(Drinks, HashStorage);
+
+function Drinks() {
+    HashStorage.call(this);
+}
+
+Drinks.prototype.update = function (key, value) {
+    if (this._hash[key]) this._hash[key] = value;
+};
+
+Drinks.prototype.has = function (key) {
+    return this._hash[key] ? 'да' : 'нет';
+};
+
+const drink = new Drinks();
+
+_inherits(Fruit, HashStorage);
+
+function Fruit() {
+    HashStorage.call(this);
+    this.box = [];
+}
+
+Fruit.prototype.addValue = function (key, value) {
+    HashStorage.prototype.addValue.apply(this, arguments);
+    this.box.push({[key]: value});
+};
+
+Fruit.prototype.number = function () {
+    return Object.keys(this._hash).length;
+};
+
+Fruit.prototype.getFruitBox = function () {
+    return this.box;
+};
+
+const fruit = new Fruit();
+
+
