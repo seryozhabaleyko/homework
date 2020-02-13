c8db59eb8a05fa9106acd8734abeb267fdd5ab7f
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-02-13 23:50:03 +0300

    Create inrange.js

---
+++ inrange.js
@@ -0,0 +1,11 @@
+const inRange = (min, max) => item => item >= min && item <= max;
+
+const arr = [0, 1, 2, 1.5, 3, 4, 5, 66, 7, 8, 17, 0.5, 9, 0, 2];
+
+arr.filter(inRange(1, 10));
+
+// function inRange(min, max) {
+//     return function (item) {
+//         return item >= min && item <= max;
+//     };
+// }
