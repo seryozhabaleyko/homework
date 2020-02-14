27fbc40aa0d87b4d123bd7c0c0b847f52d2370eb
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-02-14 17:27:56 +0300

    Update inrange.js

--- inrange.js
+++ inrange.js
@@ -2,7 +2,7 @@ const inRange = (min, max) => item => item >= min && item <= max;
 
 const arr = [0, 1, 2, 1.5, 3, 4, 5, 66, 7, 8, 17, 0.5, 9, 0, 2];
 
-arr.filter(inRange(1, 10));
+arr.filter(inRange(1, 10)); // [1, 2, 1.5, 3, 4, 5, 7, 8, 9, 2]
 
 // function inRange(min, max) {
 //     return function (item) {
