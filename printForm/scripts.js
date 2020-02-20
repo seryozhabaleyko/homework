671ebbabfaf31d7546132a0e7e54da1960c96887
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-02-21 00:00:54 +0300

    Update scripts.js

--- printForm/scripts.js
+++ printForm/scripts.js
@@ -1,10 +1,14 @@
-const fromArray1 = [
-    { label: 'Название сайта:', kind: 'text', name: 'sitename' },
-    { label: 'URL сайта:', kind: 'url', name: 'siteurl' },
-    { label: 'Посетителей в сутки:', kind: 'number', name: 'visitors' },
-    { label: 'E-mail для связи:', kind: 'email', name: 'email' },
+"use strict";
+
+const fromArray = [
+    { label: 'Разработчики:', type: 'text', name: 'dev' },
+    { label: 'Название сайта:', type: 'text', name: 'sitename' },
+    { label: 'URL сайта:', type: 'url', name: 'siteurl' },
+    { label: 'Дата запуска сайта:', type: 'date', name: 'bday' },
+    { label: 'Посетителей в сутки:', type: 'number', name: 'visitors' },
+    { label: 'E-mail для связи:', type: 'email', name: 'email' },
     {
-        label: 'Рубрика каталога:', kind: 'combo', name: 'division',
+        label: 'Рубрика каталога:', type: 'select', name: 'division',
         variants: [
             { text: 'выбрать', value: '' },
             { text: 'здоровье', value: 1 },
@@ -13,86 +17,85 @@ const fromArray1 = [
         ]
     },
     {
-        label: 'Размещение:', kind: 'radio', name: 'payment',
+        label: 'Размещение:', type: 'radio', name: 'payment',
         variants: [
             { text: 'бесплатное', value: 1 },
             { text: 'платное', value: 2 },
             { text: 'VIP', value: 3 }
         ]
     },
-    { label: 'Разрешить отзывы:', kind: 'checkbox', name: 'votes' },
-    { label: 'Описание сайта:', kind: 'memo', name: 'description' },
-    { label: 'Опубликовать:', kind: 'submit' },
-];
-
-const fromArray2 = [
-    { label: 'Фамилия:', kind: 'text', name: 'lastname' },
-    { label: 'Имя:', kind: 'text', name: 'firstname' },
-    { label: 'Отчество:', kind: 'text', name: 'secondname' },
-    { label: 'Возраст:', kind: 'number', name: 'age' },
-    { label: 'Зарегистрироваться:', kind: 'submit' },
+    { label: 'Разрешить отзывы:', type: 'checkbox', name: 'votes' },
+    { label: 'Описание сайта:', type: 'textarea', name: 'description' },
+    { label: 'Опубликовать', type: 'submit' },
 ];
 
 const form1 = document.forms.publish;
-const form2 = document.forms.register;
 
-printForm(form1, fromArray1);
-printForm(form2, fromArray2);
+printForm(form1, fromArray);
+
+function createLabel(text) {
+    const $label = document.createElement('label');
+    const $labelText = document.createTextNode(text);
+    $label.appendChild($labelText);
+    return $label;
+}
+
+function createInput(type, name, value) {
+    const $input = document.createElement('input');
+    $input.type = type;
+    $input.name = name;
+    if (value) $input.value = value;
+    return $input;
+}
+
+function createTag(tag, className) {
+    const $el = document.createElement(tag);
+    $el.className = className;
+    return $el;
+}
 
 function printForm(form, array) {
     array.forEach(item => {
-        let label = document.createElement('label');
-        let labelText = document.createTextNode(item.label);
-        label.appendChild(labelText);
+        let $group = createTag('div', 'form-group');
+        let label = createLabel(item.label);
+        let input = createInput(item.type, item.name);
 
-        let input = document.createElement('input');
-        input.type = item.kind;
-        input.name = item.name;
-
-        const group = document.createElement('div');
-        group.className = 'form-group';
-
-        if (item.kind === 'radio') {
-            group.classList.add('form-radio');
-            group.appendChild(label);
-            
-            item.variants.forEach(val => {    
-                input = document.createElement('input');
-                input.type = item.kind;
-                input.name = item.name;
-                input.value = val.value;
-                input.setAttribute('required', '');
-
-                label = document.createElement('label');
-                labelText = document.createTextNode(val.text);
-
-                group.appendChild(label).append(input, labelText);
-            });
-        } else if (item.kind === 'combo') {
-            const select = document.createElement('select');
-            select.className = 'form-control';
-            select.name = item.name;
-
-            item.variants.forEach(item => {
-                const option = document.createElement('option');
-                option.value = item.value;
-                select.appendChild(option).appendChild(document.createTextNode(item.text));
-            });
-
-            group.append(label, select);
-        } else if (item.kind === 'memo') {
-            const textarea = document.createElement('textarea');
-            textarea.name = item.name;
-            
-            group.classList.add('form-textarea');
-            group.append(label, textarea);
-        } else if (item.kind === 'submit') {
-            input.value = item.label;
-            group.appendChild(input);
-        } else {
-            group.append(label, input);
+        switch (item.type) {
+            case 'radio':
+                $group.classList.add('form-radio');
+                $group.appendChild(label);
+                item.variants.forEach(val => {
+                    const $inputRadio = createTag('div', 'input-radio');
+                    input = createInput(item.type, item.name, val.value);
+                    label = createLabel(val.text);
+                    $group.appendChild($inputRadio).append(input, label);
+                });
+                break;
+            case 'select':
+                const $select = createTag('select', 'form-control');
+                $select.name = item.name;
+                item.variants.forEach(val => {
+                    const $option = document.createElement('option');
+                    $option.value = val.value;
+                    $select.appendChild($option).appendChild(document.createTextNode(val.text))
+                });
+                $group.append(label, $select);
+                break;
+            case 'textarea':
+                const $textarea = document.createElement('textarea');
+                $textarea.name = item.name;
+                $group.classList.add('form-textarea');
+                $group.append(label, $textarea);
+                break;
+            case 'submit':
+                input.value = item.label;
+                $group.appendChild(input);
+                break;
+            default:
+                $group.append(label, input);
+                break;
         }
-
-        form.appendChild(group);
+        
+        form.appendChild($group);
     });
 }
