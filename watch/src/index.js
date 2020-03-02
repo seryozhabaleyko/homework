06620d620144970fb4a524c2d2a8ebf48f7acbc0
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-03-02 21:00:08 +0300

    Update index.js

--- watch/src/index.js
+++ watch/src/index.js
@@ -5,8 +5,10 @@ import ViewSVG from './ClockViewSVG.js';
 import Model from './Clock.js';
 import Controller from './ClockControllerButtons.js';
 
+const root = document.getElementById('root');
+
 function Watch() {
-  const view = new View();
+  const view = new View(root);
   const model = new Model(view);
   const controller = new Controller(view, model);
 }
