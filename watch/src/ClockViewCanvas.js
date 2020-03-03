934b66f4cceb45d94f9f3bb37e25bfe8246a3a17
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-03-03 15:40:02 +0300

    Update ClockViewCanvas.js

--- watch/src/ClockViewCanvas.js
+++ watch/src/ClockViewCanvas.js
@@ -1,21 +1,37 @@
-import { WATCH_WIDTH, WATCH_HEIGHT } from './Clock.js';
+'use strict';
+
+import {
+    WATCH_WIDTH, WATCH_HEIGHT,
+    WATCH_CX, WATCH_CY,
+    WATCH_RADIUS
+} from './Clock.js';
  
 class View {
-    constructor(selector) {
-        this.selector = selector;
-        this.wrapper = document.createElement('div');
-        const canvas = document.createElement('canvas');
-        canvas.width = WATCH_WIDTH;
-        canvas.height = WATCH_HEIGHT;
-        this.selector.appendChild(this.wrapper).appendChild(canvas);
-        this.ctx = canvas.getContext('2d');
-    }
 
-    createButton(name = 'button', onClick) {
-        const btn = document.createElement('button');
-        btn.innerHTML = name;
-        btn.onclick = onClick;
-        this.wrapper.appendChild(btn);
+    constructor(selector, city) {
+        this.selector = selector;
+        this.city = city;
+
+        const $wrapper = document.createElement('div');
+
+        const $canvas = document.createElement('canvas');
+        $canvas.width = WATCH_WIDTH;
+        $canvas.height = WATCH_HEIGHT;
+        
+        const $start = document.createElement('button');
+        $start.textContent = 'старт';
+        $start.addEventListener('click', () => this.startedChangeHandler(true));
+
+        const $stop = document.createElement('button');
+        $stop.textContent = 'стоп';
+        $stop.addEventListener('click', () => this.startedChangeHandler(false));
+
+        const $city = document.createElement('span');
+        $city.textContent = this.city;
+
+        this.selector.appendChild($wrapper).append($start, $stop, $city, $canvas);
+
+        this.ctx = $canvas.getContext('2d');
     }
 
     clearRect(x, y, width, height) {
@@ -49,6 +65,72 @@ class View {
         this.ctx.lineWidth = width;
         this.ctx.stroke();
         this.ctx.closePath();
+    }
+
+    render(model) {
+        this.clearRect(0, 0, WATCH_WIDTH, WATCH_HEIGHT);
+
+        this.drawCircle(WATCH_CX, WATCH_CY, WATCH_RADIUS, 'gold');
+
+        for (let i = 1; i < 13; i++) {
+            this.drawCircle(
+                model.xfrom(i),
+                model.yfrom(i),
+                30,
+                '#4caf50'
+            );
+
+            this.drawText(
+                model.xfrom(i),
+                model.yfrom(i),
+                i,
+                'black'
+            );
+        }
+
+        this.drawText(
+            WATCH_CX,
+            WATCH_CY - WATCH_HEIGHT / 5,
+            `${model.formaTime(model.date.getHours())}:${model.formaTime(model.date.getMinutes())}:${model.formaTime(model.date.getSeconds())}`,
+            'black',
+        );
+
+        this.drawLine(
+            WATCH_CX,
+            WATCH_CY,
+            model.xto(model.date.getHours(), 12, WATCH_CX, 150),
+            model.yto(model.date.getHours(), 12, WATCH_CY, 150),
+            10,
+            '#333'
+        );
+
+        this.drawCircle(WATCH_CX, WATCH_CY, 10, '#333');
+
+        this.drawLine(
+            WATCH_CX,
+            WATCH_CY,
+            model.xto(model.date.getMinutes(), 60, WATCH_CX, 170),
+            model.yto(model.date.getMinutes(), 60, WATCH_CY, 170),
+            5,
+            '#607d8b'
+        );
+
+        this.drawCircle(WATCH_CX, WATCH_CY, 8, '#607d8b');
+
+        this.drawLine(
+            WATCH_CX,
+            WATCH_CY,
+            model.xto(model.date.getSeconds(), 60, WATCH_CX, 200),
+            model.yto(model.date.getSeconds(), 60, WATCH_CY, 200),
+            3,
+            'red'
+        );
+
+        this.drawCircle(WATCH_CX, WATCH_CY, 5, 'red');
+    }
+
+    setChangeHandler(handler) {
+        this.startedChangeHandler = handler;
     }
 }
 
