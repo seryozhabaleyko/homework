84c9475e3e54a2c6f9844768cbd3ee053d85b17e
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-03-03 15:40:04 +0300

    Update index.js

--- watch/src/index.js
+++ watch/src/index.js
@@ -5,12 +5,24 @@ import ViewSVG from './ClockViewSVG.js';
 import Model from './Clock.js';
 import Controller from './ClockControllerButtons.js';
 
-const root = document.getElementById('root');
-
-function Watch() {
-  const view = new View(root);
-  const model = new Model(view);
-  const controller = new Controller(view, model);
+function watch({ selector, timezone: tz }) {  
+  const view = new View(selector, tz.string);
+  const model = new Model(tz.number);
+  const controller = new Controller(model, view);
 }
 
-Watch();
+watch({
+  selector: document.getElementById('root'),
+  timezone: {
+    number: -5,
+    string: 'Нью-Йорк (GMT-5)'
+  }
+});
+
+watch({
+  selector: document.getElementById('root'),
+  timezone: {
+    number: -1,
+    string: 'Берлин (GMT-5)'
+  }
+});
