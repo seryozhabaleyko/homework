4386df6497f6339b38e796767d0e1147111496c3
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-04-01 19:25:32 +0300

    Create index.js

---
+++ DYN_FORM_AJAX/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import Form from './Form.js';
+
+const url = 'https://fe.it-academy.by/AjaxStringStorage2.php';
+
+const getServerInformation = (name) => {
+	const options = {
+		type: 'POST',
+		url,
+		cache: false,
+		dataType: 'json',
+		async: false,
+		data: {
+			f: 'READ',
+			n: `KUZNIATSOU_DYN_FORM_AJAX_${name}`
+		},
+		success: getReady
+	};
+
+	$.ajax(options);
+};
+
+const getReady = (data) => {
+	let result = null;
+
+	try {
+		result = JSON.parse(data.result);
+	} catch (error) {
+		console.error(error);
+	}
+
+	if (result) {
+		const form = new Form(result, 'http://fe.it-academy.by/TestForm.php', 'POST');
+		form.render();
+	}
+};
+
+getServerInformation('formDef1');
+getServerInformation('formDef2');
