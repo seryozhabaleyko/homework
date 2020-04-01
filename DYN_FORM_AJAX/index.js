57162f16e5f214717823664fe0c33d29681648c0
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-04-01 19:48:21 +0300

    Update index.js

--- DYN_FORM_AJAX/index.js
+++ DYN_FORM_AJAX/index.js
@@ -5,35 +5,35 @@ import Form from './Form.js';
 const url = 'https://fe.it-academy.by/AjaxStringStorage2.php';
 
 const getServerInformation = (name) => {
-	const options = {
-		url,
-		type: 'POST',
-		cache: false,
-		dataType: 'json',
-		async: false,
-		data: {
-			f: 'READ',
-			n: `KUZNIATSOU_DYN_FORM_AJAX_${name}`
-		},
-		success: getReady
-	};
+    const options = {
+        url,
+        type: 'POST',
+        cache: false,
+        dataType: 'json',
+        async: false,
+        data: {
+            f: 'READ',
+            n: `KUZNIATSOU_DYN_FORM_AJAX_${name}`
+        },
+        success: getReady
+    };
 
-	$.ajax(options);
+    $.ajax(options);
 };
 
 const getReady = (data) => {
-	let result = null;
+    let result = null;
 
-	try {
-		result = JSON.parse(data.result);
-	} catch (error) {
-		console.error(error);
-	}
+    try {
+        result = JSON.parse(data.result);
+    } catch (error) {
+        console.error(error);
+    }
 
-	if (result) {
-		const form = new Form(result, 'http://fe.it-academy.by/TestForm.php', 'POST');
-		form.render();
-	}
+    if (result) {
+        const form = new Form(result, 'http://fe.it-academy.by/TestForm.php', 'POST');
+        form.render();
+    }
 };
 
 getServerInformation('formDef1');
